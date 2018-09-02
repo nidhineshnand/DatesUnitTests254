e20b3b49f4c63a5258fa8dcdf30f1b5f139a0200
nidhineshnand <nnan773>
2018-09-02 23:29:34 +1200

    Added nice comments for the classes and the tests

--- src/dates/test/BranchTests.java
+++ src/dates/test/BranchTests.java
@@ -9,13 +9,13 @@ import static junit.framework.TestCase.fail;
 /*It is not possible to get 100% branch coverage because there is a statement on line 92 that will be true reguardless of
 * input. This is because the boolean that defines that the statement is in the form of a enhanced for loop that will be true
 * as long as there are elements in the doomsdaysByMonth arraylist. Since the doomsdaysByMonth arraylist is set in the Dates
-* class as static private and final with 12 elements, no external tests can change the number of elemets to 0 in order to
+* class as private and final with 12 elements, no external tests can change the number of elements to 0 in order to
 * make the statement false. Hence it cannot be covered by a branch test.*/
 
 public class BranchTests {
 
-    //Leap year test that is divisible by 400 and an invalid month. This covers the branch that tests for leap year that
-    //is divisible by 400 and also goes in the branch of a invalid month (0).
+    //Leap year test check if the program can handle leap years that is divisible by 400 and has a invalid month. This
+    // covers the branch that tests for leap year that is divisible by 400 and also goes in the branch of a invalid month (0).
     @Test
     public void leapYeardivisible400InvalidMonth0(){
         try {
@@ -25,15 +25,17 @@ public class BranchTests {
 
     }
 
-    //This test going into the edge that checks if a leap year is obly divisible b 4.No other tests in the path explored
-    //this edge.
+    //This test checks if dayOfWeek can handle leap years divisible by 4. This results in the program going into the
+    // edge that checks if a leap year is only divisible by 4. No other tests this prior to this explore this path.
     @Test
     public void leapYeardivisible4(){
         assertEquals(Dates.Day.Friday, Dates.dayOfWeek(2004, 5, 21));
 
     }
 
-    //Non leap year that is divisible by 100 and invalid month
+    //This tests checks if the method is able to handle non leap years that are divisible by 100 and contain an invalid month (13).
+    //This test takes the path that covers the edge responsible for checking a non leap year and also responsible for checking
+    //if the given month is above 12 (invalid).
     @Test
     public void nonLeapYeardivisible100InvalidMonth(){
         try {
@@ -42,7 +44,9 @@ public class BranchTests {
         }catch(IllegalArgumentException e){}
     }
 
-    //Invalid year that is before 1753
+    //This test checks if the method is able to handle invalid year that is before 1753. This increases the branch
+    //coverage of the whole test suite because this test makes the program take the branch that carries out functions if
+    // the year specified is before 1753 (throws exception).
     @Test
     public void yearBefore1753(){
         try {
@@ -51,7 +55,9 @@ public class BranchTests {
         }catch(IllegalArgumentException e){}
     }
 
-    //Invalid day that is 0
+    //This test checks if the method is able to handle invalid day that is 0. This increases the branch
+    //coverage of the whole test suite because this test makes the program take the branch that carries out functions if
+    // the day specified is below 1 (throws exception).
     @Test
     public void invalidDay(){
         try {
@@ -61,7 +67,9 @@ public class BranchTests {
     }
 
 
-    //Checking if the program produces an exception if a invalid february is given
+    //This test checks if the method is able to handle invalid day for the feburary (30th Feb). This increases the branch
+    //coverage of the whole test suite because this test makes the program take the branch that carries out functions if
+    //the day for February is invalid. It throws an exception
     @Test
     public void checkInvalidFebruary(){
         try {
@@ -70,7 +78,9 @@ public class BranchTests {
         }catch(IllegalArgumentException e){}
     }
 
-    //Checking if the program produces an exception if a invalid february is given with a leap year
+    //This test checks if the method is able to handle invalid day for the leap year February (30th Feb). This increases the branch
+    //coverage of the whole test suite because this test makes the program take the branch that carries out functions if
+    //the day for February is invalid and it is a leap year.
     @Test
     public void checkInvalidFebruaryWithLeapYear(){
         try {
@@ -79,13 +89,18 @@ public class BranchTests {
         }catch(IllegalArgumentException e){}
     }
 
-    //Checking if the program produces handles a valid february input with a leap year
+
+    //This test checks if the method is able to handle valid day for the leap year February. This increases the branch
+    //coverage of the whole test suite because this test makes the program take the branch that carries out functions if
+    //the day for February is valid and it is a leap year.
     @Test
     public void checkValidFebruaryWithLeapYear(){
             assertEquals(Dates.Day.Thursday, Dates.dayOfWeek(1996, 2, 29));
     }
 
-    //Checking if the program can handle a date falling on the doomsday date.
+    //This test checks if the method is able to handle valid day tha falls on Doomsday. This increases the branch
+    //coverage of the whole test suite because this test makes the program take the branch that carries out functions if
+    //the day specified falls on or before Doomsday
     @Test
     public void checkDoomsdayDate(){
         assertEquals(Dates.Day.Friday, Dates.dayOfWeek(1999, 1, 8));
--- src/dates/test/EquivalenceTest.java
+++ src/dates/test/EquivalenceTest.java
@@ -1,7 +1,15 @@
 package dates.test;
 
-/*The non leap year divisible by 100 test was covered in branch coverage but that code exited with a exception. So a
-*good equivalence test would be to test if a year divisible by 100 is correctly identified as leap year and if the
+
+/*There are multiple classes of data that go as input in this method. The following classes are the ones that I have
+* identified for each input to the dayOfWeek method.
+* Year: Invalid year (before 1783), valid year above 1783, leap year (divisible only 4 or 400), non leap year (not
+* not divisible by 4 or 400), special non leap year (divisible by 100)
+* Month: invalid month (less than 1), valid month (between 1 and 12 inclusive) and invalid month (greater than 13)
+* Day: Invalid day (less than 0), valid day (between 0 and highest specified day of the month), invalid day ( day higher
+* then specified for that month)
+*The non leap year divisible by 100 test was covered in branch coverage but that code exited with a exception. So a
+*good equivalence test would be to test if a year divisible by 100 is correctly identified as a non leap year and if the
 * the program can give the right day for those years*/
 
 import dates.Dates;
@@ -11,10 +19,14 @@ import static junit.framework.TestCase.assertEquals;
 public class EquivalenceTest {
 
 
-    //Non leap year that is divisible by 100
+    //This test checks if the method can identify that years divisible by 100 and not by 400 are non leap years. This
+    //checks if the right day for 28 of February is specified and it also checks if the next day (1st March) is indeed
+    //the next day.
     @Test
     public void nonLeapYeardivisible100() {
-        assertEquals(Dates.Day.Monday, Dates.dayOfWeek(1900, 11, 5));
+
+        assertEquals(Dates.Day.Wednesday, Dates.dayOfWeek(1900, 2, 28));
+        assertEquals(Dates.Day.Thursday, Dates.dayOfWeek(1900, 3, 1));
     }
 
 }
